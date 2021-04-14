8a639a4d996b9f17c02a2c7fabbc8f8373bca1e9
Zulu Bravo <zulubravo@zulus-mbp.myfiosgateway.com>
2021-04-13 23:12:14 -0400

    finished miles and feet functions

--- index.js
+++ index.js
@@ -196,11 +196,9 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(km){
+    return km * 0.621371;
   }
-
-
 
 //Task 5b - Feet to CM
 /*
@@ -210,8 +208,8 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return cm/30.48;
   }
  
 
